64b89e041cbf58c4223c863fd61878b50159b6a6
Leon A. Okida Gonçalves <laog19@inf.ufpr.br>
2024-11-02 19:58:26 -0300

    add functions to handle the dht ring

--- dht_nodes.c
+++ dht_nodes.c
@@ -1,45 +1,75 @@
 #include "dht_nodes.h"
 
+// Creates a new dht node
 dht_node *create_node(int id) {
+    // Allocates memory for the dht_node structure itself
     dht_node *new_node = (dht_node *)malloc(sizeof(dht_node));
     if (!new_node) {
-        printf("Memory allocation for new node failed!");
         exit(1);
     }
+
+    // Initializes the fields
     new_node->id = id;
-    new_node->next = new_node;
+    new_node->table_size = 0;
+    new_node->finger_table_size = 0;
+
+    // Allocates memory for the table and the finger_table
+    new_node->table = (int *)malloc(32 * sizeof(int));
+    if (!new_node->table) {
+        free(new_node);
+        exit(1);
+    }
+
+    new_node->finger_table = (int *)malloc(30 * sizeof(int));
+    if (!new_node->finger_table) {
+        free(new_node->table);
+        free(new_node);
+        exit(1);
+    }
+
     return new_node;
 }
 
-void insert_node(dht_node **dht_table, int id) {
+// Function to sort the ring
+int compare_dht_nodes(const void *a, const void *b) {
+    dht_node *nodeA = *(dht_node **)a;
+    dht_node *nodeB = *(dht_node **)b;
+    return (nodeA->id - nodeB->id);
+}
+
+// Finds the address of the node with the smallest N bigger than the finger
+int find_node(int finger, dht_node **dht_table, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (dht_table[i]->id >= finger)
+            return i;
+    }
+    return 0;
+}
+
+// Updates the finger table of a node
+void update_finger_table(int node, dht_node **dht_table, int size) {
+    int m = ceil(log2(dht_table[size-1]->id));
+    int n = dht_table[node]->id;
+    int k;
+    for (k=1;k<=m;k++) {
+        // Adds the address of the node related to the finger calculated with k
+        // (N+2^(k-1))mod 2^m
+        int finger = (n + (int)pow(2, k-1)) % (int)pow(2,m);
+        dht_table[node]->finger_table[k-1] = find_node(finger, dht_table, size);
+    }
+    dht_table[node]->finger_table_size = m;
+}
+
+void insert_node(dht_node **dht_table, int *size, int id) {
     dht_node *new_node = create_node(id);
+    dht_table[*size] = new_node;
+    *size++;
+    // Sorts the ring
+    qsort(dht_table, *size, sizeof(dht_node *), compare_dht_nodes);
 
-    // Empty DHT
-    if (*dht_table == NULL) {
-        *dht_table = new_node;
-        return;
+    // Updates the finger table of each node
+    for (int node = 0; node < *size; node++) {
+        update_finger_table(node, dht_table, *size);
     }
-
-    dht_node *current = *dht_table;
-    dht_node *prev = NULL;
-
-    // The new node is smaller than the current first element
-    if (id < current->id) {
-        while (current->next != *dht_table) {
-            current = current->next;
-        }
-        current->next = new_node;
-        new_node->next = *dht_table;
-        dht_table = &(new_node);
-        return;
-    }
-
-    // Inserting at the middle or the end of the ring
-    do {
-        prev = current;
-        current = current->next;
-    } while (current != *dht_table && current->id < id);
-
-    prev->next = new_node;
-    new_node->next = current;
 }
--- dht_nodes.h
+++ dht_nodes.h
@@ -1,19 +1,20 @@
 // Leon Okida e Pedro Willian
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct dht_node dht_node;
 struct dht_node
 {
     int id;
     int *table;
+    int table_size;
     int *finger_table;
-    dht_node *next;
+    int finger_table_size;
 };
 
-
 // inserts dht node in order in the ring
-void insert_node(dht_node **dht_table, int id);
+void insert_node(dht_node **dht_table, int *size, int id);
 
 // removes node from the ring
-void remove_node(dht_node **dht_table, int id);
+void remove_node(dht_node **dht_table, int *size, int id);
